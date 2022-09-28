4cdf5ceeb707b05ead826d935c9ee37989847ace
mostafizurRahaman <fahim654326@gmail.com>
2022-09-28 23:27:47 +0600

    fake data added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+// import React, { useEffect, useState } from 'react';
+// import logo from '../../../src/utilities/logo.png'; 
+// import './Home.css'; 
+// const Home = () => {
+//       const [activities, setActivities] = useState([]); 
+//       useEffect(()=>{
+//          fetch('activities.json')
+//          .then(res => res.json())
+//          .then(data => console.log(data)); 
+//       }, [])
+//    return (
+//       <div className='home'>
+//          <div className="activities-section">
+//             <img src={logo} alt="Grow-daily-activities" className='logo' />
+//                <main className='activities-container'>
+
+//                </main>
+//          </div>
+//          <div className="sidebar">
+//             <h2>cart section</h2>
+//          </div>
+//       </div>
+//    );
+// };
+
+// export default Home;
