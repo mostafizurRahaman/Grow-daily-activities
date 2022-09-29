f8a58308bd2905d6b8f40083a3f6c7a1fb398674
mostafizurRahaman <fahim654326@gmail.com>
2022-09-29 16:46:39 +0600

    create the website is responsive

--- src/components/SuccesMessage/SuccessMessage.js
+++ src/components/SuccesMessage/SuccessMessage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SuccessMessage.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const SuccessMessage = () => {
    const notify = () =>  toast('Congratulation!! You Completed Your Activities.');
    return (
       <div>
-         <button onClick={notify}>Activities Complete</button>
+         <button onClick={notify} className="notify-button">Activities Complete</button>
          <ToastContainer></ToastContainer>
       </div>
    );
