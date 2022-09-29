68ea03246e866d7984cc7cbee4ff6fc760837cf0
mostafizurRahaman <fahim654326@gmail.com>
2022-09-29 18:14:41 +0600

    toast modified

--- src/components/SuccesMessage/SuccessMessage.js
+++ src/components/SuccesMessage/SuccessMessage.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './SuccessMessage.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Activities from '../Activities/Activities';
 const SuccessMessage = () => {
-   const notify = () =>  toast('Congratulation!! You Completed Your Activities.');
+   const notify = () => toast.success( `Congratulations!!! You are 
+   successfuly complete all Activities`, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      });
    return (
       <div>
          <button onClick={notify} className="notify-button">Activities Complete</button>
